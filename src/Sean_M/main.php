2269ac56698a89c79438b19202a41af4125fc22a
seanmcpe <seanmcpex@gmail.com>
2015-05-04 15:38:32 -0400

    Update main.php

--- src/Sean_M/main.php
+++ src/Sean_M/main.php
@@ -10,3 +10,9 @@ class Main extends PluginBase implements Listener{
      $this->getServer()->getPluginManager()->registerEvents($this);
      $this->getLogger()->info("DeathKick enabled!");
      }
+ 
+       public function onPlayerDeath(PlayerDeathEvent $event){
+       $p = $event->getPlayer();
+       $p->kick($reason="You have died!");
+       }
+}

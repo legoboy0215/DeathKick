3f3caf0ebbffd576a5afb6f98681f2f5f4a412d1
seanmcpe <seanmcpex@gmail.com>
2015-05-04 15:29:08 -0400

    Update main.php

--- src/Sean_M/main.php
+++ src/Sean_M/main.php
@@ -1,1 +1,12 @@
+<?php
+namespace Sean_M\DeathKick;
 
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener{
+
+     public function onEnable();
+     $this->getServer()->getPluginManager()->registerEvents($this);
+     $this->getLogger()->info("DeathKick enabled!");
+     }

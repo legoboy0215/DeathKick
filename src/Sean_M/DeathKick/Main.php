48e0dbbc7b73339ba97bfb950f4636b068a0e2cd
xxFlare <appletreegold@gmail.com>
2015-05-05 10:02:37 -0400

    Fixed an issue with line 11 & syntax fix
    Line 11 should of have {
    
    The syntax fix does not really change the way the code works, just makes it look better.

--- src/Sean_M/DeathKick/Main.php
+++ src/Sean_M/DeathKick/Main.php
@@ -8,15 +8,15 @@ use pocketmine\event\player\PlayerDeathEvent;
 
 class Main extends PluginBase implements Listener{
 
-     public function onEnable();
-     $this->getServer()->getPluginManager()->registerEvents($this);
-     $this->getLogger()->info("DeathKick enabled!");
+     public function onEnable(){
+          $this->getServer()->getPluginManager()->registerEvents($this);
+          $this->getLogger()->info("DeathKick enabled!");
      }
  
-       public function onPlayerDeath(PlayerDeathEvent $event){
-       $p = $event->getEntity();
-       if($p instanceof Player){
-       $p->kick($reason="You have died!");
-       }
-   }
+     public function onPlayerDeath(PlayerDeathEvent $event){
+          $p = $event->getEntity();
+          if($p instanceof Player){
+               $p->kick($reason="You have died!");
+          }
+     }
 }

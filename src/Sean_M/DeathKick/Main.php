7f21a5750307d83a29b7d2624523382186d768c9
Sean <seanmcpex@gmail.com>
2015-07-19 15:37:34 -0400

    Fix issues & start config

--- src/Sean_M/DeathKick/Main.php
+++ src/Sean_M/DeathKick/Main.php
@@ -1,15 +1,23 @@
 <?php
 namespace Sean_M\DeathKick;
 
-use pocketmine\event\Listener;
+use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Event;
+use pocketmine\event\EventPriority;
+use pocketmine\event\Listener;
+
+use pocketmine\utils\Config;
+
 use pocketmine\Player;
 use pocketmine\event\player\PlayerDeathEvent;
 
 class Main extends PluginBase implements Listener{
 
      public function onEnable(){
-          $this->getServer()->getPluginManager()->registerEvents($this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+          $this->saveDefaultConfig();
           $this->getLogger()->info("DeathKick enabled!");
      }
  
@@ -19,4 +27,3 @@ class Main extends PluginBase implements Listener{
             $p->kick($m);
      }
 }
-

f2e15a49f23b2511579f2bc184d2d2b6e9400ba0
xHFx <HFYT@openmailbox.org>
2015-08-17 07:27:31 -0400

    Update Main.php

--- src/Sean_M/DeathKick/Main.php
+++ src/Sean_M/DeathKick/Main.php
@@ -26,5 +26,6 @@ class Main extends PluginBase implements Listener{
      public function onPlayerDeath(PlayerDeathEvent $event){
         $p = $event->getEntity();
           $p->ban($this->settings['message'][That Death :(!]);
+          $this->owner->getServer()->broadcastMessage($p->getName(). "Has been banned. Reason Death!");
      }
 }
